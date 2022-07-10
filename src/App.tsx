ea5d744df6babe62d9b44521a5083330e9ac5a9d
gorazu <remypestre@gmail.com>
2022-07-10 21:21:51 +0200

    :sparkles: Add eslint and prettier config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: red;
+    font-family: Roboto;
+`;
+
+const App = () => {
+    return <Title>Hello world!</Title>;
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import "./index.css";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
